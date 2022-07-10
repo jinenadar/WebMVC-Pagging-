d46ad9d0cc49ab553f5df85be6fe924c84b0e5f5
DELL <DELL@DESKTOP-FO6L500>
2022-07-10 22:16:31 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC.Models;
+using WebMVC.Dal;
+
+namespace WebMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult insert()
+        {
+            return View();
+
+        }
+
+        public ActionResult edit()
+        {
+            return View();
+
+        }
+
+        public ActionResult delete()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebMVC.Models
+{
+    public class Categories
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
